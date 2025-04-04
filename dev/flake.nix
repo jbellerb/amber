77a8f9cce9d116103e91cabf04515d67b1e4ede3
jae beller <foss@jae.zone>
2025-04-04 13:17:14 -0400

    Add basic CI checks
    Mostly formatting. All dependencies are kept in a separate flake that's
    lazy-loaded. That way, downstream consumers avoid needing to pull them
    unless they are actually working on this flake.

---
+++ dev/flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Development dependencies for Amber";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { ... }: { };
+}
--- flake.nix
+++ flake.nix
@@ -24,19 +24,49 @@
               inherit system;
               pkgs = import nixpkgs { inherit system; };
             }
-          )
+          ) systems
         );
 
+      devHash = "sha256-Pg4La/nClq/5aD6/ykr6OBB1qDa1KHuHAxmjxXGgBfY=";
+      devPath = "path:${./dev}?narHash=${devHash}";
+      devInputs = (builtins.getFlake (builtins.unsafeDiscardStringContext devPath)).inputs;
+
+      treefmt = lib.flip devInputs.treefmt-nix.lib.evalModule (
+        { pkgs, ... }:
+        {
+          projectRootFile = "flake.nix";
+          programs.deno.enable = true;
+          programs.nixfmt.enable = true;
+          settings.global.excludes = [
+            ".envrc"
+            "LICENSE"
+            "src/module_graph.json"
+          ];
+        }
+      );
     in
     {
       mkLib = pkgs: import ./lib/default.nix { inherit (pkgs) lib newScope; };
     }
-    // perSystem (
+    // (perSystem (
       { pkgs, system }:
       {
         packages."${system}" = {
           graphAnalyzer = (self.mkLib pkgs).graphAnalyzer;
         };
+
+        devShells."${system}".default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.deno
+            self.packages."${system}".graphAnalyzer
+          ];
+        };
+
+        formatter."${system}" = (treefmt pkgs).config.build.wrapper;
+
+        checks."${system}" = {
+          formatting = (treefmt pkgs).config.build.check self;
+        };
       }
-    );
+    ));
 }
